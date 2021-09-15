4ae6ee2788047d0ccfb3a87995e1cac2787f489a
Ricardo Dollerup <ricardo.dollerup@hotmail.com>
2021-09-15 11:23:56 +0200

    Add project files.

---
+++ HotelBooking.WebApi/Extensions/DependencyRegistration.cs
@@ -0,0 +1,24 @@
+﻿using HotelBooking.Core;
+using HotelBooking.Infrastructure;
+using HotelBooking.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelBooking.WebApi.Extensions
+{
+    public static class DependencyRegistration
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<IRepository<Room>, RoomRepository>()
+                .AddScoped<IRepository<Customer>, CustomerRepository>()
+                .AddScoped<IRepository<Booking>, BookingRepository>()
+                .AddScoped<IBookingManager, BookingManager>()
+                .AddTransient<IDbInitializer, DbInitializer>();
+        }
+    }
+}
